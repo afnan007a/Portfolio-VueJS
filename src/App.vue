d15f734a73f30dc35f43a4cc36c233ec2b9e74fb
Afnan <72243953+afnan007a@users.noreply.github.com>
2021-12-02 15:34:31 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" width="25%" />
+    <code
+      ><strong><HelloWorld msg="Hey its me Vue Chan here!" /></strong
+    ></code>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #171717;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>
+      <strong>
+        I am a Full-Stack Developer and I like having friends >w<
+      </strong>
+    </h2>
+    <h2><strong>My Latest Works</strong></h2>
+    <ul>
+      <li>
+        <a href="https://github.com/afnan007a" target="_blank" rel="noopener"
+          >Github</a
+        >
+      </li>
+      <li>
+        <a href="https://gitlab.com/afnan007a" target="_blank" rel="noopener"
+          >Gitlab</a
+        >
+      </li>
+    </ul>
+    <h2><strong>Essential Links</strong></h2>
+    <ul>
+      <li>
+        <a
+          href="https://www.youtube.com/watch?v=xvFZjo5PgG0"
+          target="_blank"
+          rel="noopener"
+          >Latest Projects</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://www.youtube.com/watch?v=xvFZjo5PgG0"
+          target="_blank"
+          rel="noopener"
+          >Status Page</a
+        >
+      </li>
+    </ul>
+    <br />
+    <br />
+    <br />
+    <h3><strong>Vue Chan's Portfolio >w< </strong></h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h2 {
+  margin: 25px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  font-size: 18px;
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #d05648;
+}
+</style>
